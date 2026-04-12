3518024acaded0ff095ed0042d21db4e82d6cb5f
ARondo <arondo@Adnans-Mac-mini.local>
2026-04-12 13:23:13 -0400

    from the otherside

---
+++ src/components/MemberGrid/MemberGrid.jsx
@@ -0,0 +1,12 @@
+import MemberCard from '../MemberCard/MemberCard';
+import styles from './MemberGrid.module.css';
+
+export default function MemberGrid({ members }) {
+  return (
+    <div className={styles.grid}>
+      {members.map((member) => (
+        <MemberCard key={member.id} member={member} />
+      ))}
+    </div>
+  );
+}
